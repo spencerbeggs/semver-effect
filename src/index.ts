17f39e6948ee05b721c097b0e0893a395bd24c4d
C. Spencer Beggs <spencer@beggs.codes>
2026-03-10 17:08:43 -0400

    feat: export all error classes from barrel
    Re-export all 10 error classes from src/index.ts. Base constants
    are excluded (@internal).
    
    Signed-off-by: C. Spencer Beggs <spencer@beggs.codes>

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,18 @@
  *
  * @packageDocumentation
  */
+
+export { EmptyCacheError } from "./errors/EmptyCacheError.js";
+export { InvalidBumpError } from "./errors/InvalidBumpError.js";
+export { InvalidComparatorError } from "./errors/InvalidComparatorError.js";
+export { InvalidPrereleaseError } from "./errors/InvalidPrereleaseError.js";
+export { InvalidRangeError } from "./errors/InvalidRangeError.js";
+// Errors -- Parsing
+export { InvalidVersionError } from "./errors/InvalidVersionError.js";
+// Errors -- Constraint
+export { UnsatisfiableConstraintError } from "./errors/UnsatisfiableConstraintError.js";
+// Errors -- Resolution
+export { UnsatisfiedRangeError } from "./errors/UnsatisfiedRangeError.js";
+// Errors -- Fetch
+export { VersionFetchError } from "./errors/VersionFetchError.js";
+export { VersionNotFoundError } from "./errors/VersionNotFoundError.js";
