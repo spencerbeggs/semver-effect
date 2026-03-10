8a95475d87114ee70f4486fb98e889ecff0a2eec
C. Spencer Beggs <spencer@beggs.codes>
2026-03-10 17:54:53 -0400

    chore: update build config
    Signed-off-by: C. Spencer Beggs <spencer@beggs.codes>

--- rslib.config.ts
+++ rslib.config.ts
@@ -1,6 +1,7 @@
 import { NodeLibraryBuilder } from "@savvy-web/rslib-builder";
 
 export default NodeLibraryBuilder.create({
+	externals: ["effect"],
 	transform({ pkg, target }) {
 		if (target?.registry === "https://npm.pkg.github.com/") {
 			pkg.name = "@spencerbeggs/semver-effect";
@@ -9,6 +10,7 @@ export default NodeLibraryBuilder.create({
 		delete pkg.bundleDependencies;
 		delete pkg.scripts;
 		delete pkg.publishConfig;
+		delete pkg.packageManager;
 		delete pkg.devEngines;
 		return pkg;
 	},
